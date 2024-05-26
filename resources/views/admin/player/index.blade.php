aafaca78a7ee9a03241b89b92661d9cc15d256c8
sophia <thangneimawi@aceplussolutions.com>
2024-05-26 22:03:49 +0630

    add report

--- resources/views/admin/player/index.blade.php
+++ resources/views/admin/player/index.blade.php
@@ -106,6 +106,7 @@
                   <i class="fas fa-right-left text-white me-1"></i>
                   Logs
                 </a>
+                
                 <a href="{{ route('admin.transferLogDetail', $user->id) }}" data-bs-toggle="tooltip" data-bs-original-title="Reports" class="btn btn-info btn-sm">
                   <i class="fas fa-right-left text-white me-1"></i>
                   transferLogs
