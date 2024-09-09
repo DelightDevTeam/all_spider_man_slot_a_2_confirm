d402f60f01426b3f54a2abb99ae8bbd08f623b0e
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-09-09 09:09:36 +0630

    site updated

--- app/Http/Middleware/CheckPlayerBan.php
+++ app/Http/Middleware/CheckPlayerBan.php
@@ -14,10 +14,31 @@ class CheckPlayerBan
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+//     public function handle(Request $request, Closure $next): Response
+// {
+//     if (Auth::check() && Auth::user()->status == 0) {
+//         Auth::logout();
+//         return response()->json([
+//             'message' => 'You are banned. Please contact the administrator for more information.'
+//         ], 403); // Forbidden status
+//     }
+
+//     return $next($request);
+// }
+
     public function handle(Request $request, Closure $next): Response
 {
     if (Auth::check() && Auth::user()->status == 0) {
+        // Logout the user
         Auth::logout();
+
+        // Revoke/Delete the user's tokens
+        $user = $request->user();
+        if ($user->tokens()) {
+            $user->tokens()->delete();
+        }
+
+        // Return a JSON response indicating the user is banned
         return response()->json([
             'message' => 'You are banned. Please contact the administrator for more information.'
         ], 403); // Forbidden status
@@ -25,5 +46,6 @@ class CheckPlayerBan
 
     return $next($request);
 }
+
 
 }
