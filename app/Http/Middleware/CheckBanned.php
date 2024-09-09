f0d9823ab7f9e8e7e6bd866f47c44a59b0860665
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-09-09 08:37:10 +0630

    site updated

--- app/Http/Middleware/CheckBanned.php
+++ app/Http/Middleware/CheckBanned.php
@@ -14,12 +14,24 @@ class CheckBanned
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+    // public function handle(Request $request, Closure $next): Response
+    // {
+    //     if (Auth::check() && Auth::user()->status == 0 ) {
+    //         Auth::logout(); 
+    //         return redirect()->route('login')->with('error', 'You are banned. Please contact the administrator for more information.');
+    //     }
+    //     return $next($request);
+    // }
+
     public function handle(Request $request, Closure $next): Response
-    {
-        if (Auth::check() && Auth::user()->status == 0 ) {
-            Auth::logout(); 
-            return redirect()->route('login')->with('error', 'You are banned. Please contact the administrator for more information.');
-        }
-        return $next($request);
+{
+    // Ensure the authenticated user is an agent and banned (status == 0)
+    if (Auth::check() && Auth::user()->status == 2) {
+        Auth::logout();
+        return redirect()->route('admin.login')->with('error', 'You are banned. Please contact the administrator.');
     }
+
+    return $next($request);
+}
+
 }
